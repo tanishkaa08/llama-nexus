69a7e91f75424747774864d3fc7cfd946057dfa6
Xin Liu <sam@secondstate.io>
2025-06-07 13:30:40 +0800

    refactor!: replace `MCP_TOOLS` and ` MCP_CLIENTS` with `USER_TO_MCP_TOOLS` and `USER_TO_MCP_CLIENT`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/mcp.rs
+++ src/mcp.rs
@@ -6,14 +6,20 @@ use rmcp::{
 use std::collections::HashMap;
 use tokio::sync::RwLock as TokioRwLock;
 
-pub static MCP_TOOLS: OnceCell<TokioRwLock<HashMap<String, McpClientName>>> = OnceCell::new();
-pub static MCP_CLIENTS: OnceCell<TokioRwLock<HashMap<McpClientName, TokioRwLock<McpClient>>>> =
-    OnceCell::new();
+pub static USER_TO_MCP_TOOLS: OnceCell<
+    TokioRwLock<HashMap<UserId, TokioRwLock<HashMap<McpToolName, McpClientName>>>>,
+> = OnceCell::new();
+pub static USER_TO_MCP_CLIENTS: OnceCell<
+    TokioRwLock<HashMap<UserId, TokioRwLock<HashMap<McpClientName, TokioRwLock<McpClient>>>>>,
+> = OnceCell::new();
+
 pub static MCP_VECTOR_SEARCH_CLIENT: OnceCell<TokioRwLock<McpClient>> = OnceCell::new();
 pub static MCP_KEYWORD_SEARCH_CLIENT: OnceCell<TokioRwLock<McpClient>> = OnceCell::new();
 
 pub type RawMcpClient = RunningService<RoleClient, Box<dyn DynService<RoleClient>>>;
 pub type McpClientName = String;
+pub type McpToolName = String;
+pub type UserId = String;
 
 #[allow(dead_code)]
 pub struct McpClient {
